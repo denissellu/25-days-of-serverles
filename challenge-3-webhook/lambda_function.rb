73cead5d7371e4284cbfd26fcaf84060d35cdd40
denissellu <hingasellu@gmail.com>
2019-12-11 23:22:18 +0000

    Add a way to get files commited

--- challenge-3-webhook/lambda_function.rb
+++ challenge-3-webhook/lambda_function.rb
@@ -3,8 +3,13 @@ require 'json'
 def handler(event:, context:)
   # TODO: implement
   #
-  pp event
-  pp context
+  push_events = JSON.parse(event.body)
+
+  push_events.commits.each do |c|
+    pp '-'*50
+    pp c
+    pp '-'*50
+  end
 
   { statusCode: 200, body: JSON.generate('Hello from Ruby Lambda') }
 end
